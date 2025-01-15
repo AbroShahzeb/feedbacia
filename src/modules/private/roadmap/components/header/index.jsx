d3a70f6904f6b1ca673eec7781463742437517d0
AbroShahzeb <khizr77@gmail.com>
2025-01-15 21:48:30 +0500

    Fixed a bug

--- src/modules/private/roadmap/components/header/index.jsx
+++ src/modules/private/roadmap/components/header/index.jsx
@@ -5,17 +5,17 @@ export const Header = () => {
   return (
     <div className="p-6 md:px-8 bg-grey-500 flex items-center gap-3 justify-between md:rounded-[10px] md:h-[113px] max-w-[1110px] mx-auto">
       <div>
-        <Link to="/add-feedback">
-          <Button
-            variant="back-transparent"
-            label="Go back"
-            style={{ color: "#fff" }}
-          />
-        </Link>
+        <Button
+          variant="back-transparent"
+          label="Go back"
+          style={{ color: "#fff" }}
+        />
         <p className="text-xl text-grey-0 font-bold md:text-3xl">Roadmap</p>
       </div>
 
-      <Button label="+ Add Feedback" />
+      <Link to="/add-feedback">
+        <Button label="+ Add Feedback" />
+      </Link>
     </div>
   );
 };
