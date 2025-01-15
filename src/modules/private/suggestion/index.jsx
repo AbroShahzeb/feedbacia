5c27e234c60b4d197e5165b97c73beda5d8ee0c7
AbroShahzeb <khizr77@gmail.com>
2025-01-15 22:51:23 +0500

    Changes

--- src/modules/private/suggestion/index.jsx
+++ src/modules/private/suggestion/index.jsx
@@ -13,13 +13,13 @@ export const Suggestion = () => {
 
   return (
     <main className="w-full min-h-screen bg-grey-50">
-      <div className="xl:max-w-[1110px] xl:px-0 xl:mx-auto md:flex md:flex-col md:gap-10 xl:flex-row xl:items-start xl:gap-[30px] md:px-10 md:pt-[56px] md:pb-[113px] xl:pt-[94px] xl:pb-[129px]">
+      <div className="xl:max-w-[1110px] xl:px-0 xl:mx-auto md:flex md:flex-col md:gap-10 xl:flex-row xl:items-start xl:gap-[15px] md:px-10 md:pt-[56px] md:pb-[113px] xl:pt-[94px] xl:pb-[129px]">
         <Sidebar />
 
         <MobileNav />
 
         {/* Main Body Start */}
-        <div className="flex flex-col gap-6 w-full">
+        <div className="flex flex-col gap-6 w-full flex-1">
           <Header sortBy={sortBy} setSortBy={setSortBy} />
           {suggestions && suggestions.length > 0 ? (
             <FeedbackList feedbacks={suggestions} />
