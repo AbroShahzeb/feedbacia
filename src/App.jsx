2267e5a7106f92eadfb3fda669c0c728218158da
AbroShahzeb <khizr77@gmail.com>
2025-01-27 00:56:20 +0500

    Solved dark mode issue and setup react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
 import { AppRouter } from "./routes/AppRouter";
 
 function App() {
+  useEffect(() => {
+    const isToggled = localStorage.getItem("theme") === "dark";
+    if (isToggled) {
+      localStorage.setItem("theme", "dark");
+      document.getElementById("body").classList.add("dark");
+    } else {
+      localStorage.setItem("theme", "light");
+      document.getElementById("body").classList.remove("dark");
+    }
+  }, []);
   return (
     <div>
       <AppRouter />
