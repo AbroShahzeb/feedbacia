73f7242260ccac761be5b0bf9ca02d091e64f7f0
AbroShahzeb <khizr77@gmail.com>
2025-01-27 13:54:05 +0500

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,14 @@ function App() {
     }
   }, []);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get(`https://feedbacia-api.vercel.app`);
+      console.log(response);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div>
       <Toaster
